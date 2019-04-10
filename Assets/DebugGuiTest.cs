551525c62e65d0e6dc1e943aa07a34a99ea17e0b
fastsystem <info@fast-system.jp>
2019-04-10 19:48:00 +0900

    init

---
+++ Assets/DebugGuiTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugGuiTest : MonoBehaviour
+{
+    DebugGui debugGui;
+
+    void Start()
+    {
+        debugGui = FindObjectOfType<DebugGui>();
+    }
+
+    void Update()
+    {
+        debugGui.Log("Info");
+        debugGui.LogWarning("Warning");
+        debugGui.LogError("Error");
+    }
+}
